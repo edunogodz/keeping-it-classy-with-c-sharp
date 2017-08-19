6833c084d14395e454d614268170a8dadcd13e24
edunogodz <edunogodz@gmail.com>
2017-08-19 15:04:01 -0300

    Lesson 2

---
+++ Band.cs
@@ -0,0 +1,11 @@
+using System;
+public class Band
+{
+ public string Name;
+ public int Musicians;
+
+ public void Announce()
+ {
+    Console.WriteLine("Welcome " + Name + " to the stage!");
+ }
+}
